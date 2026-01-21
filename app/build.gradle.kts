e816cb394f71352e5bf5ff2a48ac1c2f626c12d3
Yu2002s <jiangdongyu54@gmail.com>
2026-01-21 14:01:40 +0800

    feat: 更新 live-2.jsonc

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
     minSdk = 23
     //noinspection ExpiredTargetSdkVersion
     targetSdk = 28
-    versionCode = 5
-    versionName = "1.0.5"
+    versionCode = 6
+    versionName = "1.0.6"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     signingConfig = signingConfigs.getByName("debug")
